cac66b46a6df19b87c8c5551f64e876e8858b251
Prithwee Das <prithweedas123@gmail.com>
2019-07-10 15:11:24 +0530

    refresh token

--- flaskrest/api/auth/routes.py
+++ flaskrest/api/auth/routes.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify
+from flask import request, jsonify, make_response
 
 from flaskrest.api import api_blueprint
 from flaskrest.api.auth import user_schema, users_schema
@@ -25,8 +25,12 @@ def login():
     user = User.query.filter_by(email=request.json['email']).first()
 
     if user and bcrypt.check_password_hash(user.password, request.json['password']):
-        token = create_token(user)
-        return jsonify({'ok': True, 'user': user_schema.dump(user).data, 'token': token})
+        token, refresh_token = create_token(user)
+        res = make_response(
+            jsonify({'ok': True, 'user': user_schema.dump(user).data}))
+        res.set_cookie("x-access-token", value=token)
+        res.set_cookie("x-access-refresh-token", value=refresh_token)
+        return res
 
     return jsonify({'ok': False, 'error': 'Wrong email or password'})
 
