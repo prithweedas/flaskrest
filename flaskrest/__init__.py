55c65be11c020abaa991803cc808c07fb5f0eccb
Prithwee Das <prithweedas123@gmail.com>
2019-07-10 13:47:49 +0530

    creating password and hashing

--- flaskrest/__init__.py
+++ flaskrest/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['FLASK_BLOG_DB_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['FLASK_REST_DB_URL']
+app.config['SECRET_KEY'] = os.environ['FLASK_REST_SECRET']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+bcrypt = Bcrypt(app)
 
 from flaskrest.api import api_blueprint
 
 app.register_blueprint(api_blueprint)
+
+
+from flaskrest.api.auth import models
--- flaskrest/api/auth/routes.py
+++ flaskrest/api/auth/routes.py
@@ -1,8 +1,34 @@
-from flask import jsonify
+from flask import request, jsonify
 
 from flaskrest.api import api_blueprint
+from flaskrest.api.auth import user_schema, users_schema
+from flaskrest.api.auth.models import User
+from flaskrest import db, bcrypt
+
+'''
+Every route in this file will start with '/auth'
+'''
 
 
-@api_blueprint.route('/auth/')
-def index():
-    return jsonify({'ok': True})
+@api_blueprint.route('/auth/register', methods=['POST'])
+def create_user():
+    hashed_password = bcrypt.generate_password_hash(request.json['password'])
+    new_user = User(request.json['email'], hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'ok': True, 'user': user_schema.dump(new_user).data})
+
+
+@api_blueprint.route('/auth/login', methods=['POST'])
+def login():
+    user = User.query.filter_by(email=request.json['email']).first()
+    if user and bcrypt.check_password_hash(user.password, request.json['password']):
+        return jsonify({'ok': True, 'user': user_schema.dump(user).data})
+
+    return jsonify({'ok': False, 'error': 'Wrong email or password'})
+
+
+@api_blueprint.route('/auth/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return users_schema.jsonify(users)
