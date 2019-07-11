b257cdc46309df2e0a6db48090900b849198306c
Prithwee Das <prithweedas123@gmail.com>
2019-07-11 12:58:50 +0530

    delete todo

--- flaskrest/api/todos/routes.py
+++ flaskrest/api/todos/routes.py
@@ -17,9 +17,21 @@ def get_all_todos(current_user):
 @api_blueprint.route('/todo/create', methods=['POST'])
 @check_token
 def create_todo(current_user):
-    print(request.json)
     todo = Todo(request.json['title'],
                 request.json['description'], current_user)
     db.session.add(todo)
     db.session.commit()
     return jsonify({'ok': True, 'todo': todo_schema.dump(todo).data})
+
+
+@api_blueprint.route('/todo/delete/<int:id>', methods=['POST'])
+@check_token
+def delete_todo(current_user, id):
+    todo = Todo.query.filter_by(id=id).first()
+    if not todo:
+        return jsonify({'ok': False, 'error': "Todo doesn't exist"})
+    if todo.owner.id != current_user.id:
+        return jsonify({'ok': False, 'error': "You don't own this todo"}), 401
+    db.session.delete(todo)
+    db.session.commit()
+    return jsonify({'ok': True})
